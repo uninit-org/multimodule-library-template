3c833bcc9267c8161693ed40da4fa44229c06b54
Luna <imlvnaa@gmail.com>
2023-11-30 09:03:00 -0500

    Actually suppress it

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import java.text.SimpleDateFormat
 import java.util.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
@@ -22,8 +23,10 @@ buildscript {
 
 allprojects {
 
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
-        kotlinOptions { freeCompilerArgs += "-Xexpect-actual-classes" }
+    tasks.withType(KotlinCompile::class).all {
+        kotlinOptions {
+            freeCompilerArgs += "-Xexpect-actual-classes"
+        }
     }
 
     group = "uninit"
